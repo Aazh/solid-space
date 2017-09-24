c6b0acb732970dc96ff1eff5d1aa33cc34235e65
Aazh <maiste.martin@gmail.com>
2017-09-24 15:30:20 +0300

    OK

--- balltracker.py
+++ balltracker.py
@@ -8,24 +8,26 @@ while(1):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     # define range of blue color in HSV
     lower_orange = np.array([0,140,140])
-    upper_orange = np.array([45,255,255])
+    upper_orange = np.array([40,255,255])
     # Threshold the HSV image to get only orange colors
     mask = cv2.inRange(hsv, lower_orange, upper_orange)
     # Bitwise-AND mask and original image
     res = cv2.bitwise_and(frame,frame, mask= mask)
-    try:
-        M = cv2.moments(mask)
-        cx = int(M['m10'] / M['m00'])
-        cy = int(M['m01'] / M['m00'])
-        cv2.circle(res, (cx, cy), 5, (0, 0, 255), -1)
-    except:
-        M = 0
+    im2, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    if len(contours) != 0:
+        # draw in blue the contours that were founded
+        cv2.drawContours(res, contours, -1, 255, 3)
+
+        # find the biggest area
+        c = max(contours, key=cv2.contourArea)
+
+        x, y, w, h = cv2.boundingRect(c)
+        # draw the book contour (in green)
+        cv2.rectangle(res, (x, y), (x + w, y + h), (0, 255, 0), 2)
     cv2.imshow("mask",mask)
     cv2.imshow('frame',frame)
     cv2.imshow('res',res)
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
-print(cx)
-print(cy)
 cv2.destroyAllWindows()
