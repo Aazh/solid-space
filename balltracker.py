e33c99f38c040c8273ce2b0dccaa0c85b470357f
Aazh <maiste.martin@gmail.com>
2017-09-24 15:07:33 +0300

    OK

--- balltracker.py
+++ balltracker.py
@@ -7,16 +7,25 @@ while(1):
     # Convert BGR to HSV
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     # define range of blue color in HSV
-    lower_orange = np.array([0,130,130])
+    lower_orange = np.array([0,140,140])
     upper_orange = np.array([45,255,255])
-    # Threshold the HSV image to get only blue colors
+    # Threshold the HSV image to get only orange colors
     mask = cv2.inRange(hsv, lower_orange, upper_orange)
     # Bitwise-AND mask and original image
     res = cv2.bitwise_and(frame,frame, mask= mask)
+    try:
+        M = cv2.moments(mask)
+        cx = int(M['m10'] / M['m00'])
+        cy = int(M['m01'] / M['m00'])
+        cv2.circle(res, (cx, cy), 5, (0, 0, 255), -1)
+    except:
+        M = 0
+    cv2.imshow("mask",mask)
     cv2.imshow('frame',frame)
-    cv2.imshow('mask',mask)
     cv2.imshow('res',res)
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
+print(cx)
+print(cy)
 cv2.destroyAllWindows()
