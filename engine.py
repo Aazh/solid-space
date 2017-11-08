624b27e82d1a02c076620e35ab0af8d6c373fabc
Aazh <maiste.martin@gmail.com>
2017-11-08 20:10:57 +0200

    robot liigub paremini,0.2 ms aeg elektroonikasse saatmiste vahel

--- engine.py
+++ engine.py
@@ -1,7 +1,9 @@
 from detection_functions import detector
+
+
 from movement_functions import liigu, viska
 from math import pi
-from time import time
+from time import *
 import cv2
 import serial
 
@@ -21,7 +23,7 @@ def main():
         bytesize=serial.EIGHTBITS,
         timeout=0
     )
-    ser.write('d:1000'.encode())
+    ser.write('d:1000\n'.encode())
     t = time() + 2
     #state = 'stop'
     state = 'search and destroy'
@@ -38,10 +40,10 @@ def main():
 
 
         while True:
-            input = str(ser.readline()).split(':')[-1]
+            """input = str(ser.readline()).split(':')[-1]
             if input != "b''":
                 input = input[:-4]
-                print(input)
+                #print(input)
             if input == 'a{0}PING-----'.format(RobotID):
                 ser.write("rf:a{0}ACK------\n".format(RobotID).encode())
             if input == 'a{0}START----'.format(RobotID):
@@ -54,7 +56,7 @@ def main():
                 state = 'search and destroy'
             if input == 'a{0}XSTOP-----'.format(FieldID):
                 state = 'stop'
-            input = None
+            input = None"""
 
 
 
@@ -72,59 +74,78 @@ def main():
                         stopbits=serial.STOPBITS_TWO,
                         bytesize=serial.EIGHTBITS
                     )
-
+                #ser.write('fs:0\n'.encode())
                 ret, mask, x, y, area = detectors[0].detect(cap)
                 cv2.imshow('kontroll', ret)
                 print(x, y)
-                if time() < q:
-                    liigu(-0.4, 9 / 6 * pi, 0, ser)
-                    rotate = time() + 4
-                elif y > 420 and y != -1:
+                #if time() < q:
+                #    print('1')
+                #    liigu(-0.4, 9 / 6 * pi, 0, ser)
+                #    rotate = time() + 4
+                if y > 420 and x > 300 and x < 360:
+                    print('2')
                     state = 'kill ball'
-                    cannontime = time()+5
-                    liigu(0, 0, 0, ser)
-                elif x > 300 and x < 360 and x != -1 and area > 10:
+                    #cannontime = time()+5
+
+                    #liigu(0, 0, 0, ser)
+                elif x > 300 and x < 360 and x != -1 and area > 50:
                     print('eh')
                     if y < 420 and y != -1:
-                        q = time() + 0.15
+                        print('if')
+                        liigu(-0.4, 9 / 6 * pi, 0, ser)
+                        #q = time() + 0.15
                         rotate = time() + 4
 
                     else:
+                        print('else')
                         liigu(0, 0, 0, ser)
 
                 else:
-
+                    print('else2')
                     if x < 300 and x != -1 and area > 10:
+                        print('siin')
                         liigu(0, 0, -1, ser)
                         rotate = time() + 4
 
                     elif x > 360 and x != -1 and area > 10:
+                        print('seal')
                         liigu(0, 0, 1, ser)
                         rotate = time() + 4
 
                     elif time() > rotate:
+                        print('mujal')
                         liigu(0, 0, 1, ser)
 
 
                     else:
+                        print('see')
                         liigu(0, 0, 0, ser)
 
             if state == 'kill ball':
-                liigu(-0.2, 9 / 6 * pi, 0, ser)
-                viska(1500, ser)
-                if cannontime < time():
-                    state = 'search and destroy'
+                a = time() + 2
+                viska(1400, ser)
+                while time() < a:
+                    liigu(-0.2, 9 / 6 * pi, 0, ser)
+
+                state = 'search and destroy'
+            else:
+                viska(1000, ser)
+            print(state)
 
             if k == 27:
                 break
 
         cv2.destroyAllWindows()
         cap.release()
+        liigu(0, 0, 0, ser)
         ser.close()
     except:
+        print('except')
         cv2.destroyAllWindows()
         cap.release()
+        liigu(0, 0, 0, ser)
         ser.close()
+
 
 
 if __name__ == '__main__':
