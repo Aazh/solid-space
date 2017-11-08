10727632fbd1351a490e8c6ddb0da33d8f261fc0
Aazh <maiste.martin@gmail.com>
2017-11-08 20:11:30 +0200

    0.2 ms aeg elektroonikasse saatmiste vahel

--- movement_functions.py
+++ movement_functions.py
@@ -1,4 +1,5 @@
 from math import pi, cos
+from time import time
 import serial
 
 wheelConfig0 = [11/6*pi , 0.135]
@@ -26,12 +27,40 @@ def mainboardSpeedCalc(wheelLinearVelocity):
     return int(wheelAngularSpeedMainboardUnits)
 
 def liigu(speed, angle, angularVelocity, ser):
+    global liigu_kontroll
+    global liigu_aeg
     wheelSpeed0 = str(mainboardSpeedCalc(wheelCalc(0, speed, angle, angularVelocity)))
     wheelSpeed1 = str(mainboardSpeedCalc(wheelCalc(1, speed, angle, angularVelocity)))
     wheelSpeed2 = str(mainboardSpeedCalc(wheelCalc(2, speed, angle, angularVelocity)))
-
-    ser.write("sd:{0}:{1}:{2}\n".format(wheelSpeed0, wheelSpeed1, wheelSpeed2).encode())
+    liigu_uus = "sd:{0}:{1}:{2}\n".format(wheelSpeed0, wheelSpeed1, wheelSpeed2).encode()
+    try:
+        if liigu_uus == liigu_kontroll:
+            if time() > liigu_aeg:
+                liigu_aeg = time() + 0.2
+                ser.write(liigu_uus)
+        else:
+            liigu_aeg = time() + 0.2
+            liigu_kontroll = liigu_uus
+            ser.write(liigu_uus)
+    except:
+        liigu_aeg = time() + 0.2
+        liigu_kontroll = liigu_uus
+        ser.write(liigu_uus)
 
 def viska(x, ser):
-    x = 1500
-    ser.write('d:{0}'.format(x).encode())
+    global viska_kontroll
+    global viska_aeg
+    viska_uus = 'd:{0}'.format(x).encode()
+    try:
+        if viska_uus == viska_kontroll:
+            if time() > viska_aeg:
+                viska_aeg = time() + 0.2
+                ser.write(viska_uus)
+        else:
+            viska_aeg = time() + 0.2
+            viska_kontroll = viska_uus
+            ser.write(viska_uus)
+    except:
+        viska_aeg = time() + 0.2
+        viska_kontroll = viska_uus
+        ser.write(viska_uus)
